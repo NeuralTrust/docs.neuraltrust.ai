51a8194d0bee44c821ca7ff0d0f8ea7b9b0b5f7c
Victor <victor.garcia@singularly.com>
2024-12-05 15:22:43 +0100

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://neuraltrust.github.io/docs',
+  url: 'https://neuraltrust.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NeuralTrust', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'neuraltrust', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
